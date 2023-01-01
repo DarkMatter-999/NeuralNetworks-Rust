ccbe9569abd4258e7436ff7a6eb6dc8353a605df
DarkMatter-999 <darkmatter999official@gmail.com>
2023-01-01 11:00:07 +0530

    successfully trained a NN gfor XOR operation

--- src/main.rs
+++ src/main.rs
@@ -14,39 +14,47 @@ fn main() {
         Box::new(Sigmoid::new(1, 1)),
     ];
 
-    let x = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]];
-    let y = [[0.0], [1.0], [1.0], [0.0]];
+    let xtrain = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]];
+    let ytrain = [[0.0], [1.0], [1.0], [0.0]];
+
+    let epochs = 10000;
+    let learning_rate = 1.0;
 
     let mut error = 0.0;
 
-    for (x, y) in x.into_iter().zip(y.into_iter()) {
-        // forward
-        let mut output = Matrix::new(2, 1);
-        for i in 0..output.rows {
-            output.data[i][0] = x[i];
+    for e in 0..epochs {
+        for (x, y) in xtrain.into_iter().zip(ytrain.into_iter()) {
+            // forward
+            let mut output = Matrix::new(2, 1);
+            for i in 0..output.rows {
+                output.data[i][0] = x[i];
+            }
+
+            let mut y_ = Matrix::new(1, 1);
+            y_.data[0][0] = y[0];
+
+            for layer in &mut network {
+                output = layer.forward(output);
+            }
+
+            // println!("{:?} => {:?} / {:?}", x, y, &output.data);
+
+            // error
+            error += mse(&y_, &output);
+
+            //backward
+            let mut grad = mse_prime(&y_, &output);
+
+            // println!("grad: {:?}", grad.data);
+
+            network.reverse();
+            for layer in &mut network {
+                grad = layer.backward(grad, learning_rate);
+            }
+            network.reverse();
         }
 
-        let mut y_ = Matrix::new(1, 1);
-        y_.data[0][0] = y[0];
-
-        for layer in &mut network {
-            output = layer.forward(output);
-        }
-
-        // println!("{:?} => {:?} / {:?}", x, y, &output.data);
-
-        // error
-        error += mse(&y_, &output);
-
-        //backward
-        let mut grad = mse_prime(&y_, &output);
-
-        println!("grad: {:?}", grad.data);
-
-        network.reverse();
-        for layer in &mut network {
-            grad = layer.backward(grad, 0.1);
-        }
-        network.reverse();
+        error /= xtrain.len() as f64;
+        println!("{:}/{:} error={:}", e + 1, epochs, error);
     }
 }
